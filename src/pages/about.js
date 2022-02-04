19a59de5bc223d92bbf73c0f089220a9a3fb808c
Damiano31 <damianoresta56@gmail.com>
2022-02-04 17:37:11 +0100

    bo

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+// Step 1: Import React
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="about page" honor="o bo non lo so lo sai jiahane?">
+        <p>prova di p</p>
+    </Layout>
+  )
+}
+
+
+// Step 3: Export your component
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,21 @@
 // Step 1: Import React
 import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+
+
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Page</title>
-      <h1>Welcome to my Gatsby site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+    <Layout pageTitle="HOME jiahane" honor="SOTD">
+      <p>pindex p</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+    </Layout>
+
   )
 }
 // Step 3: Export your component
